19685ab87b484257bad2ba3f27ef062bc95a2bf9
Jerryasrat <Eyerusalem.asrat28@gmail.com>
2022-12-11 07:55:45 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'c';
+	while
+		(a <= 'b') {
+			putchar(a);
+		}
+	putchar('\n');
+	return (0);
+}	
