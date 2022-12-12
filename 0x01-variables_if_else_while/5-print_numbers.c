858a2da8e7c252cb41015fc1714d412360339e30
Jerryasrat <Eyerusalem.asrat28@gmail.com>
2022-12-12 04:57:18 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit nubers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+		for(c = '0'; c <= '9'; c++)
+			putchar(c);
+
+	putchar('\n');
+	return (0);
+}	
