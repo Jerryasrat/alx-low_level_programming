825b6e4e757edba620b7ec5e347483c9a13a2eac
Jerryasrat <Eyerusalem.asrat28@gmail.com>
2022-12-13 10:32:46 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
-/**
+/*
  * main - Entry
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
+	_putchar('_');
 	_putchar('\n');
+	_putchar('p');
 	_putchar('r');
 	_putchar('a');
 	_putchar('h');
 	_putchar('c');
 	_putchar('t');
 	_putchar('u');
-	_putchar('p');
-	_putchar('_');
 
 	return (0);
 }
+	
