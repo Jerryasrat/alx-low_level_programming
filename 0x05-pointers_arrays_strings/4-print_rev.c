8b681908424e615ba95732f35987e9a972037cc8
Jerryasrat <Eyerusalem.asrat28@gmail.com>
2022-12-19 11:47:25 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main .h"
+#include <string.h>
+/**
+ *print_rev -> print a string in reverse
+ *@s: reversed string
+ *
+ */
+void print_rev(char *s);
+{
+	int n;
+
+	n = strlen(s);
+	for (i = n-1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
