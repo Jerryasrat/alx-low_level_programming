238ae33e9c94c13418830ece211bf65c62c068ff
Jerryasrat <Eyerusalem.asrat28@gmail.com>
2023-01-05 08:39:53 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,17 @@
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: the string to be printed.
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+		
 	{
-		_putchar('\0');
+		_putchar('\n');
 		return;
 	}
+
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
