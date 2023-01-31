15466b517e36039d6fd9f95d4a72b99bb8dab8fd
Jerryasrat <Eyerusalem.asrat28@gmail.com>
2023-01-30 23:48:50 -0800

    looped_listint_count

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -41,3 +41,42 @@ size_t looped_listint_count(listint_t *head)
 	return (0);
 }
 
+/**
+ * free_listint_safe - frees a listint_t list safely
+ * @h: a pointer to the address
+ *
+ * Return: the size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t nodes, index;
+
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
+	{
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
+}
